99214b425774f877e4dc2288303e9649a41fa54b
Walczakp007 <walczakp007@gmail.com>
2019-01-01 20:17:42 +0100

    First commit

---
+++ solver.hs
@@ -0,0 +1,13 @@
+readWords path = do
+  words <- readFile path
+  let processedWords = ignoreIrrevelantSigns (lines words)
+  return processedWords
+
+removePunc :: String -> String
+removePunc xs = [ x | x <- xs, not (x `elem` ",.?!-:;\"\'") && x /= ' ']
+ignoreIrrevelantSigns :: [String] -> [String]
+ignoreIrrevelantSigns [] = []
+ignoreIrrevelantSigns (x:xs) = removePunc x : ignoreIrrevelantSigns xs
+
+
+lol = readWords "/home/paul/SPOP/data2/words"
