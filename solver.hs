2591a4c84b1a83a989848a53a6fba2e0b4260b76
KamilaLis <kamila.lis@outlook.com>
2019-01-13 13:24:43 +0100

    fixing logic: init

--- solver.hs
+++ solver.hs
@@ -1,5 +1,5 @@
 import Data.List
-
+import Data.Char
 -- usun znaki interpunkcyjne (nie powinny wystapic)
 removePunc :: String -> String
 removePunc xs = [ x | x <- xs, not (x `elem` ",.?!-:;\"\'") && x /= ' ']
@@ -50,8 +50,8 @@ diagonal [] = []
 diagonal ([]:_) = []
 diagonal ((x):xs) = head x : diagonal (map tail xs)
 
--- 
-antidiagonal :: [[(Char, Int)]] 
+--
+antidiagonal :: [[(Char, Int)]]
               -> [(Char, Int)]
 antidiagonal [] = []
 antidiagonal ([]:_) = []
@@ -66,41 +66,18 @@ getAllCombinations :: [[(Char, Int)]] -> [[(Char, Int)]]
 getAllCombinations [[]] = []
 getAllCombinations crossword = crossword ++ transposeCrossword crossword ++ allDiagonals crossword ++ allDiagonals (transposeCrossword crossword)
 
-printElements :: [(Char, Int)] -> Int -> Int -> IO()
-printElements [] _ _  = putStrLn ""
-printElements ((a,b):c) 0 crwrdLength = do
-    putStrLn([a])
-    printElements c crwrdLength crwrdLength
-
-printElements ((a,b):c) num crwrdLength = do
-  putStr([a])
-  printElements c (num - 1) crwrdLength
-
-printList :: String ->  IO()
-printList [] = do putStrLn ""
-printList (x:xs) = do putStr [x]
-                      printList xs
-
-printSolution :: [String] -> IO()
-printSolution [] = do putStr ""
-printSolution (x:xs) = do printList x
-                          printSolution xs
-
-
 findString' :: String          -- ^ string to search for
             -> String          -- ^ string to search in
             -> Maybe Int       -- ^ starting index
-findString' _ [] = Nothing
-findString' [] _ = Nothing
 findString' search str = findIndex (isPrefixOf search) (tails str)
 
--- 
+test = findString "CDE" [('A', 1),('B', 2),('C' ,3),('C',4),('E',5),('F', 6)]
+
+--
 findString :: String           -- ^ string to search for
             -> [(Char, Int)]   -- ^ row to search in
             -> Maybe [Int]     -- ^ list of indexes
-findString _ [] = Nothing
-findString [] _ = Nothing
-findString search str = do 
+findString search str = do
   idx <- findString' search (map fst str)
   return (map snd . take (length search) . drop idx $ str)
 
@@ -109,30 +86,28 @@ findWord :: String              -- ^ word
           -> [[(Char, Int)]]    -- ^ all combinations of crossword
           -> Maybe [Int]        -- ^ list of indexes
 findWord _ [] = Nothing
-findWord [] _ = Nothing
-findWord word [x] = findString word x
-findWord word (x:xs) = if (findString word x) == Nothing 
+findWord word (x:xs) = if (findString word x) == Nothing
                         then findWord word xs
                       else findString word x
 
 removeLetter' :: [(Char, Int)] -> Int -> [(Char, Int)]
 removeLetter' [] _ = []
-removeLetter' [(a,b)] idx = []
-removeLetter' ((a,b):c) idx | b == idx = c
+removeLetter' ((a,b):c) idx | b == idx = [('*',b)] ++ c
                             | otherwise = [(a,b)] ++ (removeLetter' c idx)
 
--- Remove letter with given index form crossword 
+-- Remove letter with given index form crossword
 removeLetter :: [[(Char, Int)]]  -- ^ crossword
               -> Int             -- ^ index of letter to remove
               -> Int             -- ^ number of cols in crossword
+              -> Int
               -> [[(Char, Int)]] -- ^ updated crossword
-removeLetter [] _ _ = []   
-removeLetter [x] idx cols = [removeLetter' x idx]          
-removeLetter (x:xs) idx cols | idx < 0 = (x:xs)
-                             | idx < cols = ((removeLetter' x idx): xs)
-                             | otherwise = (x:(removeLetter xs idx (cols+cols)))
+removeLetter [] _ _ _= []
+removeLetter [x] idx cols _ = [removeLetter' x idx]
+removeLetter (x:xs) idx cols colsConst | idx < 0 = (x:xs)
+                                       | idx <= cols = ((removeLetter' x idx): xs)
+                                       | otherwise = (x: (removeLetter xs idx (cols+colsConst+1) colsConst) )
 
--- -- Remove letters with given list of numbers form crossword 
+-- -- Remove letters with given list of numbers form crossword
 -- removeWord :: [[(Char, Int)]]    -- ^ crossword
 --               -> Maybe [Int]     -- ^ indexes of letter to remove
 --               -> Int             -- ^ number of cols in crossword
@@ -142,21 +117,20 @@ removeLetter (x:xs) idx cols | idx < 0 = (x:xs)
 --   Just n -> removeWord (removeLetter cross (head n) cols) (Just (tail n)) cols
 --   Nothing -> cross
 
--- Remove letters with given list of numbers form crossword 
+-- Remove letters with given list of numbers form crossword
 removeWord :: [[(Char, Int)]]    -- ^ crossword
               -> [Int]           -- ^ indexes of letter to remove
               -> Int             -- ^ number of cols in crossword
               -> [[(Char, Int)]] -- ^ updated crossword
 removeWord cross [] cols = cross
-removeWord cross [x] cols = removeLetter cross x cols
-removeWord cross (x:xs) cols = removeWord (removeLetter cross x cols) xs cols
+removeWord cross (x:xs) cols = removeWord (removeLetter cross x cols cols) xs cols
 
 -- Solve the crossword
 solve :: [[(Char, Int)]]  -- ^ crossword
       -> [String]         -- ^ list of words to find
       -> [[(Char, Int)]]  -- ^ solution
 solve cross [] = cross
-solve cross [w] = 
+solve cross [w] =
   case findWord w (getAllCombinations cross) of
     Just n -> removeWord cross n 11
     Nothing -> error w
@@ -167,14 +141,17 @@ solve cross (w:ws) = solve (solve cross [w]) ws
 --     Just n -> removeWord cross n 11
 --     Nothing -> error "world not found"
 
+makeCrossString :: [[(Char, Int)]] -> String
+makeCrossString [] = []
+makeCrossString (x:xs) = makeCrossString' x ++ "\n" ++ makeCrossString xs
+
+makeCrossString' :: [(Char, Int)] -> String
+makeCrossString' [] = []
+makeCrossString' ((a,b):xs) = a : makeCrossString' xs
+
 main :: IO ()
 main = do
   cross <- readCrossword "data1/crossword"
-  words <- readWords "data1/words" 
-  -- print words
-  -- print (getAllCombinations cross)
-  -- print (solve cross ["BRIDE", "CARDS","CARESS", "CHOCOLATE"])
-  -- print (removeWord (removeWord cross "ARROW" 11) "ADORER" 11)
-  print (solve cross ["ADORER", "ARROW"])
-
--- ["ADORER","ARROW","BOUQUET","BRIDE","CARDS","CARESS","CHOCOLATE","COUPLE","CUPID","DATE","DATING","DEVOTION","EMBRACE","FIRST","KISS","GROOM","HEART","HUGS","ISEULT","LOVE","LUCK","LYRE","ODE","POEM","PRESENT","QUEEN","RENDEZVOUS","RING","ROMEO","ROSES","RYE","SCENTS","SENSE","SONG","SWEET","TRISTAN","WEDDING"]
+  words <- readWords "data1/words"
+  print (solve cross ["JULIET", "CARESS","WEDDING", "ROMEO", "BOUQUETTE"])
+  --print ( handle (solve cross ["JULIET", "CARESS"]) ([0..(16*12)]) (length(head cross)))
